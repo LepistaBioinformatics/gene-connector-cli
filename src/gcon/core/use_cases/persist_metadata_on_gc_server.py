eeede15f2f49cbd16c331c71af68697cd2ce2398
Samuel Galvão Elias <sgelias@outlook.com>
2023-05-02 21:29:36 -0300

    docs: documenting the unimplemented persist-medatada-on-gc-server use-case with todo
    main

---
+++ src/gcon/core/use_cases/persist_metadata_on_gc_server.py
@@ -0,0 +1,9 @@
+"""
+
+TODO: Implement this use case
+
+This use case is responsible for persisting the metadata to the Gene Connector
+remote server. While the server is not implemented yet, this use case will
+not be available.
+
+"""
